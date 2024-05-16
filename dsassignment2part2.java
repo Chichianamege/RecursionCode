49cba18fd5520187a4549e89cad7216d07282fad
jake <130408715+Chichianamege@users.noreply.github.com>
2024-05-15 21:31:40 -0400

    Update dsassignment2part2.java

--- dsassignment2part2.java
+++ dsassignment2part2.java
@@ -1,8 +1,5 @@
 // Name: Chidera Anamege
-// Class:	CS 3305 W01
-// Term:	Spring 2024
-//	Instructor:  Carla McManus
-//	Assignment:  2 – Part 2 Recursion
+
 import java.util.Scanner;
 
 public class dsassignment2part2 {
